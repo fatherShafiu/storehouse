e127ca23500584e6cd9c08b83e4f424faba3a2c9
Tonon Abdul-Shafiu Mahmud <tononabdulshafiumahmud@gmail.com>
2023-10-15 05:49:15 +0000

    added product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif\jpg\png)\z}i,
+    message: 'must be a URL  for GIF, JPG OR PNG image.'
+  }
 end
