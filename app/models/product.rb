e28036f13f363a6f29010d46b97012dc08e9f959
Tonon Abdul-Shafiu Mahmud <tononabdulshafiumahmud@gmail.com>
2023-10-20 00:35:32 +0000

    added views test, and store index controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,8 @@ class Product < ApplicationRecord
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
 
   validates :image_url, allow_blank: true, format: {
-    with: %r{\.(gif\jpg\png)\z}i,
-    message: 'must be a URL  for GIF, JPG OR PNG image.'
+    with: %r{\.(gif|jpg|png)\z}i,
+    message: 'must be a URL for GIF, JPG, or PNG image.'
   }
+
 end
